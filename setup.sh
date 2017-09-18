da032bdde11c4fcdace0c0b9ef1d3fb1563370b6
Sher Sanginov <sanginovs@berea.edu>
2017-09-18 17:49:59 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+sudo apt-get install -y \
+    python-pip \
+    build-essential \
+    git \
+    python \
+    python-dev
+
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
+if [ ! -d venv ]
+then
+  virtualenv venv
+fi
+
+
+#Installing MongoDB:
+sudo apt-get install -y mongodb-org
+
+#To start MongoDB:
+mongod -f mongod.conf
+
+. venv/bin/activate
+pip install -r requirments.txt
