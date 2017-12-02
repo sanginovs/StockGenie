5a5506441d42785b58f98adb9d7638a96786ea4e
asharron <alexsharron@gmail.com>
2017-12-02 17:04:17 -0500

    Edited the prediction.py to have a function for prediction

--- prediction.py
+++ prediction.py
@@ -2,7 +2,17 @@ import keras
 import numpy as np
 model = keras.models.load_model('trained_model.h5')
 
-X = [[22.05,25.22,21.23,23.43]]
-X = np.asarray(X,dtype=np.float32)
-a = model.predict(X)
-print(a)
+def test():
+    """Tests to see if the model is working """
+    X = [22.05,25.22,21.23,23.43]
+    X = [X]
+    X = np.asarray(X,dtype=np.float32)
+    a = model.predict(X)
+    print(a)
+
+def predictDay(x):
+    """Takes the data provided and makes a prediction for each day provided"""
+    x = [x]
+    x = np.asarray(x,dtype=np.float32)
+    result = model.predict(x)
+    return result
